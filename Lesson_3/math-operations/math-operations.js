30b172f3baa4ede36ba292fa33fa2fc423c88079
JuliaLuccio <julialuccio@gmail.com>
2022-10-30 18:39:04 +0200

    add lesson_3 math operations

---
+++ Lesson_3/math-operations/math-operations.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+   const firstNumber = Number(prompt('Введіть перше число для розрахунку'));
+   const secondNumber = Number(prompt('Введіть друге число для розрахунку'));
+
+   const resultAddTwoNum = firstNumber + secondNumber;
+   const resultSubTwoNum = firstNumber - secondNumber;
+   const resultMultTwoNum = firstNumber * secondNumber;
+   const resultDivTwoNum = firstNumber / secondNumber;
+
+   if (firstNumber !== firstNumber || secondNumber !== secondNumber) {
+      alert(`Ви ввели не число! Спробуйте ще раз.`);
+   } else {
+      alert(`Користувач ввів ${firstNumber} і ${secondNumber}:
+      ${firstNumber} + ${secondNumber} = ${resultAddTwoNum}
+      ${firstNumber} - ${secondNumber} = ${resultSubTwoNum}
+      ${firstNumber} * ${secondNumber} = ${resultMultTwoNum}
+      ${firstNumber} / ${secondNumber} = ${resultDivTwoNum}`);
+   };
+})
