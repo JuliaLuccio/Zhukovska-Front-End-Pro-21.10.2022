da4381bd51b04456c06aa19f6a0bad70417129bc
JuliaLuccio <julialuccio@gmail.com>
2022-11-06 15:18:05 +0200

    magic num, move Cyrillic, add lower case checks

--- Lesson_4/user-portait.js
+++ Lesson_4/user-portait.js
@@ -1,25 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
    const userYearOfBirth = Number(prompt('Введіть Ваш рік народження'));
-   const userCity = prompt('В якому місті Ви живете?').toLowerCase();
-   const userFavoriteSport = prompt('Ваш улюблений вид спорту?').toLowerCase();
+   let userCity = prompt('В якому місті Ви живете?');
+   let userFavoriteSport = prompt('Ваш улюблений вид спорту?');
 
-   const userAge = 2022 - userYearOfBirth;
+   const currentYear = new Date().getFullYear();
+   const maxAgeOfLife = 122;
+   const minValidYear = currentYear - maxAgeOfLife;
+   const userAge = currentYear - userYearOfBirth;
+
+   if (userCity && userCity[0] === userCity[0].toLowerCase()) {
+      userCity = userCity.replace(userCity[0], userCity[0].toUpperCase());
+   }
+
+   const kyiv = 'Київ';
+   const washington = 'Вашингтон';
+   const london = 'Лондон';
 
    const capitalOfCountry = {
-      київ: 'України',
-      вашингтон: 'США',
-      лондон: 'Великобританії',
+      [kyiv]: 'України',
+      [washington]: 'США',
+      [london]: 'Великобританії'
    };
 
    const userCountry = capitalOfCountry[userCity];
    let userPlaceOfLive = userCountry
       ? `столиці ${userCountry}`
-      : `місті ${userCity.replace(userCity[0], userCity[0].toUpperCase())}`;
+      : `місті ${userCity}`;
+
+   if (userFavoriteSport && userFavoriteSport[0] === userFavoriteSport[0].toUpperCase()) {
+      userFavoriteSport = userFavoriteSport.replace(userFavoriteSport[0], userFavoriteSport[0].toLowerCase());
+   }
+
+   const tennis = 'теніс';
+   const boxing = 'бокс';
+   const football = 'футбол';
 
    const sportAndSportsChampions = {
-      теніс: 'Сестри Серена та Вінус Вільямс',
-      бокс: 'Олександр Усик',
-      футбол: 'Андрій Шевченко',
+      [tennis]: 'Сестри Серена та Вінус Вільямс',
+      [boxing]: 'Олександр Усик',
+      [football]: 'Андрій Шевченко'
    };
 
    const sportsChampions = sportAndSportsChampions[userFavoriteSport];
@@ -27,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
       ? `Круто! Мрієте стати як ${sportsChampions}?`
       : `Ваш улюблений вид спорту ${userFavoriteSport}`;
 
-   let messageUserAge = userYearOfBirth > 1900 && userYearOfBirth < 2022
+   let messageUserAge = userYearOfBirth > minValidYear && userYearOfBirth < currentYear
       ? `Схоже Вам у цьому році виповнилось ${userAge} 🎉`
       : 'Шкода, що Ви не захотіли ввести свій рік народження';
 
@@ -39,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
       ? userSportChampions
       : 'Шкода, що Ви не захотіли вказати улюблений спорт';
 
-   !(userYearOfBirth > 1900 && userYearOfBirth < 2022) && !userCity && !userFavoriteSport
+   !(userYearOfBirth > minValidYear && userYearOfBirth < currentYear) && !userCity && !userFavoriteSport
       ? alert('Шкода, що Ви нічого не вказали!')
       : alert(`${messageUserAge} \n${messageUserPlaceOfLive}. \n${messageUserChampion}`);
 });
