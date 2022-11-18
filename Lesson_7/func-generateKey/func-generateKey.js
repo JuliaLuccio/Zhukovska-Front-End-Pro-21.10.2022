03f8b820610287a3e3633f61436f3b8563fea17d
JuliaLuccio <julialuccio@gmail.com>
2022-11-19 01:49:50 +0200

    les 7 generate key

--- Lesson_7/func-generateKey/func-generateKey.js
+++ Lesson_7/func-generateKey/func-generateKey.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+   const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+   const charLen = characters.length;
+
    const generateKey = (length, characters) => {
-      
+      let result = '';
+      for (let i = 0; i < length; i++) {
+         const randomNum = Math.floor(Math.random() * charLen);
+         result += characters.charAt(randomNum);
+      }
+      return result;
    }
+
+   const key = generateKey(16, characters);
+   console.log(key);
 })
